5eec28354fe1a20d4fb1d10c283d8ff5afacd026
Mathias Bøhn Grytemark <mathias@grytemark.no>
2013-10-15 00:04:30 +0200

    added example settings files

---
+++ settings_local.example.py
@@ -0,0 +1,26 @@
+
+
+DEBUG = False
+
+
+#ADMINS = (
+#	 ('Mr. Your Name', 'email@example.com'),
+#)
+#MANAGERS = ADMINS
+
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'challenge',
+		'USER': 'challenge',
+		'PASSWORD': 'challenge',
+		'HOST': '127.0.0.1',
+		'PORT': '',
+	}
+}
+
+
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 't+q))2q)e6k6*ggeqw*bl9q3%_t-(e3#%!v$yl-l(s^rtabf!)'
