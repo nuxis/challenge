8339daf652be0bc6337650d72b20c9f265abc1ab
Mathias Bøhn Grytemark <mathias@grytemark.no>
2013-10-19 23:55:26 +0200

    more fixes for django version

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url, include
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,17 +6,15 @@ admin.autodiscover()
 from django.conf import settings
 
 urlpatterns = patterns('',
-	(r'^admin/', include(admin.site.urls)),
-
-	(r'^$', 'challenge.levels.views.index'),
-	(r'^done/$', 'challenge.levels.views.done'),
+	url (r'^admin/', include(admin.site.urls)),
+	url (r'^$', 'challenge.levels.views.index'),
+	url (r'^done/$', 'challenge.levels.views.done'),
 	
-	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+	url (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
-	(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'core/login.html'}),
-	(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
-	(r'^register/$', 'challenge.core.views.register'),
-	(r'^score/$', 'challenge.stats.views.score'),
-	(r'^attempts/$', 'challenge.stats.views.attempts'),
-	
+	url (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'core/login.html'}),
+	url (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+	url (r'^register/$', 'challenge.core.views.register'),
+	url (r'^score/$', 'challenge.stats.views.score'),
+	url (r'^attempts/$', 'challenge.stats.views.attempts'),
 )
