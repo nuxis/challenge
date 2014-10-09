f6f400e46b8274c172a7282540a6fb6c818be5e7
Kristoffer Dalby <kradalby@kradalby.no>
2014-10-09 20:11:03 +0200

    changed urls

--- challenge/urls.py
+++ challenge/urls.py
@@ -7,15 +7,15 @@ from django.conf import settings
 
 urlpatterns = patterns('',
 	url (r'^admin/', include(admin.site.urls)),
-	url (r'^$', 'challenge.levels.views.index'),
-	url (r'^done/$', 'challenge.levels.views.done'),
+	url (r'^$', 'levels.views.index'),
+	url (r'^done/$', 'levels.views.done'),
 	
 	url (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
 	url (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'core/login.html'}),
 	url (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
-	url (r'^register/$', 'challenge.core.views.register'),
-	url (r'^score/$', 'challenge.stats.views.score'),
-	url (r'^attempts/$', 'challenge.stats.views.attempts'),
-	url (r'^attempts/(?P<getnum>\d*)/$', 'challenge.stats.views.attempts'),
+	url (r'^register/$', 'core.views.register'),
+	url (r'^score/$', 'stats.views.score'),
+	url (r'^attempts/$', 'stats.views.attempts'),
+	url (r'^attempts/(?P<getnum>\d*)/$', 'stats.views.attempts'),
 )
