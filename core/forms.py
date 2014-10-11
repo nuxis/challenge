ad5feb05151b4c2fdead1a48742124dcb578dca1
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-10-12 00:41:56 +0200

    fixing indenting and line length

--- core/forms.py
+++ core/forms.py
@@ -3,9 +3,10 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 class UserForm(Form):
-	first_name = forms.CharField (max_length=32, label=_("Firstname"))
-	last_name = forms.CharField (max_length=64, label=_("Lastname"))
-	username = forms.CharField (max_length=32, label=_("Username"))
-	email = forms.CharField (max_length=128, label=_("Email"))
-	password = forms.CharField (widget=forms.PasswordInput, label=_("Password"))
-	password2 = forms.CharField (widget=forms.PasswordInput, label=_("Repeat passord"))
+    first_name = forms.CharField(max_length=32, label=_("Firstname"))
+    last_name = forms.CharField(max_length=64, label=_("Lastname"))
+    username = forms.CharField(max_length=32, label=_("Username"))
+    email = forms.CharField(max_length=128, label=_("Email"))
+    password = forms.CharField(widget=forms.PasswordInput, label=_("Password"))
+    password2 = forms.CharField(widget=forms.PasswordInput,
+        label=_("Repeat passord"))
