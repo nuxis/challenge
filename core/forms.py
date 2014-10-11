f5b0aa64f99910e242e116c22200b8d91ab4ef31
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-10-11 22:10:29 +0200

    Translated register-form

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,11 @@
 from django.forms import Form
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 class UserForm(Form):
-	first_name = forms.CharField (max_length=32, label="Fornavn")
-	last_name = forms.CharField (max_length=64, label="Etternavn")
-	username = forms.CharField (max_length=32, label="Nick")
-	email = forms.CharField (max_length=128, label="Email")
-	password = forms.CharField (widget=forms.PasswordInput, label="Passord")
-	password2 = forms.CharField (widget=forms.PasswordInput, label="Gjenta passord")
+	first_name = forms.CharField (max_length=32, label=_("Firstname"))
+	last_name = forms.CharField (max_length=64, label=_("Lastname"))
+	username = forms.CharField (max_length=32, label=_("Username"))
+	email = forms.CharField (max_length=128, label=_("Email"))
+	password = forms.CharField (widget=forms.PasswordInput, label=_("Password"))
+	password2 = forms.CharField (widget=forms.PasswordInput, label=_("Repeat passord"))
