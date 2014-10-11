919b29f02d1ab9c12640edeb26a350a42b2c95ef
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-10-11 21:59:03 +0200

    Translated the rest of register.html and made the errors a bit nicer.

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
 from core.forms import UserForm
 
@@ -18,23 +20,27 @@ def register(request):
 	data = request.POST.copy()
 	c['form'] = form = UserForm(data)
 
-	if not form.is_valid():
-		c['error'] = True
-		return render (request, "core/register.html", c)
+	error = False
 
+	if not form.is_valid():
+		messages.error (request, _("Something went wrong, check the form and try again"))
+		error = True
 
 	if form.cleaned_data['password'] != form.cleaned_data['password2']:
-		c['password_mismatch'] = True
-		return render (request, "core/register.html", c)
+		messages.error (request, _("The passwords you entered did not match"))
+		error = True
 		
 	if User.objects.filter(username__iexact=form.cleaned_data['username']).count() > 0:
-		c['user_exists'] = True
+		messages.error (request, _("Username already exists"))
+		error = True
+
+	if error:
 		return render (request, "core/register.html", c)
 
 	try:
 		user = User.objects.create_user (form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
 	except:
-		c['user_exists'] = True
+		messages.error (request, _("Username already exists"))
 		return render (request, "core/register.html", c)
 	
 	user.first_name = form.cleaned_data['first_name']
