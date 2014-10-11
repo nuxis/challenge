60bd0e73f2c73744751973ed0d56f16734519802
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-10-12 00:16:52 +0200

    Fixing whitespace and indenting per PEP8

--- levels/models.py
+++ levels/models.py
@@ -2,27 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Level(models.Model):
-	name = models.CharField(max_length=64)
-	question = models.TextField()
-	multianswer = models.BooleanField(default=False)
-	answer = models.CharField(max_length=128)
-	sourcehint = models.CharField (max_length=256, blank=True)
-	imageurl = models.CharField (max_length=256, blank=True)
-	buttontext = models.CharField (max_length=256, blank=True)
-	css = models.TextField (blank=True)
+    name = models.CharField(max_length=64)
+    question = models.TextField()
+    multianswer = models.BooleanField(default=False)
+    answer = models.CharField(max_length=128)
+    sourcehint = models.CharField(max_length=256, blank=True)
+    imageurl = models.CharField(max_length=256, blank=True)
+    buttontext = models.CharField(max_length=256, blank=True)
+    css = models.TextField(blank=True)
 
-	def __str__(self):
-		return str(self.pk) + " - " + self.name
+    def __str__(self):
+        return str(self.pk) + " - " + self.name
 
 
 class Score(models.Model):
-	user = models.ForeignKey(User, unique=True, editable=False)
-	max_level = models.IntegerField()
-	updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, unique=True, editable=False)
+    max_level = models.IntegerField()
+    updated = models.DateTimeField(auto_now=True)
 
 class Attempt(models.Model):
-	user = models.ForeignKey (User)
-	level = models.ForeignKey (Level)
-	answer = models.TextField ()
-	correct = models.BooleanField (default=False)
-	time = models.DateTimeField (auto_now=True)
+    user = models.ForeignKey(User)
+    level = models.ForeignKey(Level)
+    answer = models.TextField()
+    correct = models.BooleanField(default=False)
+    time = models.DateTimeField(auto_now=True)
