ff18c6de47970b28d33e9a701329473045495f8e
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-10-11 21:39:01 +0200

    Adding names to urls

--- challenge/urls.py
+++ challenge/urls.py
@@ -7,15 +7,15 @@ from django.conf import settings
 
 urlpatterns = patterns('',
 	url (r'^admin/', include(admin.site.urls)),
-	url (r'^$', 'levels.views.index'),
-	url (r'^done/$', 'levels.views.done'),
+	url (r'^$', 'levels.views.index', name='index'),
+	url (r'^done/$', 'levels.views.done', name='done'),
 	
 	url (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
 	url (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'core/login.html'}, name="login"),
 	url (r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout_then_login"),
 	url (r'^register/$', 'core.views.register', name="register"),
-	url (r'^score/$', 'stats.views.score'),
-	url (r'^attempts/$', 'stats.views.attempts'),
+	url (r'^score/$', 'stats.views.score', name='score'),
+	url (r'^attempts/$', 'stats.views.attempts', name='attempts'),
 	url (r'^attempts/(?P<getnum>\d*)/$', 'stats.views.attempts'),
 )
