f319d6969a7174e86c8971321421810aec7207f3
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-10-12 00:44:53 +0200

    Fixing whitespace

--- challenge/urls.py
+++ challenge/urls.py
@@ -6,19 +6,19 @@ admin.autodiscover()
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url (r'^admin/', include(admin.site.urls)),
-    url (r'^$', 'levels.views.index', name='index'),
-    url (r'^done/$', 'levels.views.done', name='done'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'levels.views.index', name='index'),
+    url(r'^done/$', 'levels.views.done', name='done'),
 
-    url (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}),
 
-    url (r'^login/$', 'django.contrib.auth.views.login',
+    url(r'^login/$', 'django.contrib.auth.views.login',
         {'template_name': 'core/login.html'}, name="login"),
-    url (r'^logout/$', 'django.contrib.auth.views.logout_then_login',
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
         name="logout_then_login"),
-    url (r'^register/$', 'core.views.register', name="register"),
-    url (r'^score/$', 'stats.views.score', name='score'),
-    url (r'^attempts/$', 'stats.views.attempts', name='attempts'),
-    url (r'^attempts/(?P<getnum>\d*)/$', 'stats.views.attempts'),
+    url(r'^register/$', 'core.views.register', name="register"),
+    url(r'^score/$', 'stats.views.score', name='score'),
+    url(r'^attempts/$', 'stats.views.attempts', name='attempts'),
+    url(r'^attempts/(?P<getnum>\d*)/$', 'stats.views.attempts'),
 )
