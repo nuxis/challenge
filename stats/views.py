a8598af8e4c95b2d6941b1fe10712a9673b223d3
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-10-11 23:59:32 +0200

    Fixing indenting and whitespace

--- stats/views.py
+++ stats/views.py
@@ -4,23 +4,18 @@ from django.shortcuts import render
 
 from levels.models import Score, Attempt
 
-
 # score can be accessed by anyone
 def score (request):
-	c = {}
-	c['score'] = Score.objects.order_by ('-max_level', 'updated')
-
-	return render (request, "score.html", c)
-
+    c = {}
+    c['score'] = Score.objects.order_by ('-max_level', 'updated')
+    return render (request, "score.html", c)
 
 # attemps should only be visible to superusers
 @user_passes_test(lambda u: u.is_superuser)
 def attempts (request, getnum=None):
-	if getnum == None:
-		getnum = 25
-	
-	c = {}
-	c['getnum'] = getnum
-	c['attempts'] = Attempt.objects.order_by('-pk')[:getnum]
-
-	return render (request, "attempts.html", c)
+    if getnum == None:
+        getnum = 25
+    c = {}
+    c['getnum'] = getnum
+    c['attempts'] = Attempt.objects.order_by('-pk')[:getnum]
+    return render (request, "attempts.html", c)
