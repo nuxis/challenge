d3fe98fa4e763bce599dea7697a377cd46a3a828
Mathias Bøhn Grytemark <mathias@grytemark.no>
2016-05-15 23:05:30 +0200

    fix site configuration using django-solo

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from core.models import Config
+from solo.admin import SingletonModelAdmin
 from django.contrib import admin
 
-admin.site.register(Config)
+admin.site.register(Config, SingletonModelAdmin)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from solo.models import SingletonModel
+from django.utils.translation import ugettext as _
 
-class Config(models.Model):
+class Config(SingletonModel):
     welcometext = models.TextField(blank=True)
     eventname = models.CharField(max_length=256)
     active = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return _("Site configuration")
+
+    class Meta:
+         verbose_name = _("Site configuration")
