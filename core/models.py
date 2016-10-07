526e729ba15add2a4f62f58c2bc5cb5b4e73c6dd
Mathias Bøhn Grytemark <mathias@grytemark.no>
2016-10-07 18:10:14 +0200

    add untested changes for listing latest correct answer

--- core/models.py
+++ core/models.py
@@ -26,6 +26,10 @@ class UserProfile(models.Model):
     def get_score(self):
         return Score.objects.filter(user=self.user).aggregate(Sum('points'))['points__sum']
 
+    def latest_correct_answer(self):
+        latest = Score.objects.filter(user=self.user).latest('awarded')
+        return latest.awarded
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, signal, created, **kwargs):
