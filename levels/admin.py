9db4c2b5ea5483e631b6f37cdd4c58213fe8e724
Mathias Bøhn Grytemark <mathias@grytemark.no>
2016-10-07 19:08:46 +0200

    add untested hiding of levels

--- levels/admin.py
+++ levels/admin.py
@@ -2,6 +2,6 @@ from levels.models import Level
 from django.contrib import admin
 
 class LevelAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'name', 'description', 'points', 'point_requirement']
+    list_display = ['pk', 'name', 'description', 'points', 'required_points']
 
 admin.site.register(Level, LevelAdmin)
