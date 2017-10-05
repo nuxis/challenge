0d48f4ff35525331182ca25b595ab658ef15bff9
Mathias Bøhn Grytemark <mathias@grytemark.no>
2017-10-05 18:37:04 +0200

    add inline admin for userprofile
    need this to reset my own score when testing

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,27 @@
 from core.models import Config
 from solo.admin import SingletonModelAdmin
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from .models import UserProfile
 
 admin.site.register(Config, SingletonModelAdmin)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'UserProfile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
